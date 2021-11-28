70624bc053bc4ae62d86cd917da046f535f169d4
Csaba Pap <csabapapdev@gmail.com>
2021-11-28 15:04:00 +0100

    metal initialization, display clear color

---
+++ HelloTriangle/TriangleShaders.metal
@@ -0,0 +1,18 @@
+//
+//  TriangleShaders.metal
+//  HelloTriangle
+//
+//  Created by Csaba Pap on 2021. 11. 28..
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+vertex float4 base_vertex_function() {
+    return float4(1);
+}
+
+fragment half4 base_fragment_function() {
+    return half4(1);
+}
+
--- HelloTriangle/TriangleView.swift
+++ HelloTriangle/TriangleView.swift
@@ -10,4 +10,52 @@ import MetalKit
 
 class TriangleView: MTKView {
     
+    var commandQueue: MTLCommandQueue!
+    var renderPipelineState: MTLRenderPipelineState!
+
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.device = MTLCreateSystemDefaultDevice()
+        
+        self.clearColor = MTLClearColor(red: 0.4, green: 0.8, blue: 0.4, alpha: 1)
+        self.colorPixelFormat = .bgra8Unorm
+        
+        self.commandQueue = device?.makeCommandQueue()
+        
+        createPipelineState()
+    }
+    
+    func createPipelineState() {
+        let library = device?.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "base_vertex_function")
+        let fragmentFunction = library?.makeFunction(name: "base_fragment_function")
+        
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.vertexFunction = vertexFunction
+        renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        do {
+            try renderPipelineState = device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else {
+            return
+        }
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        commandEncoder?.setRenderPipelineState(renderPipelineState)
+        commandEncoder?.endEncoding()
+        
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+    }
 }
